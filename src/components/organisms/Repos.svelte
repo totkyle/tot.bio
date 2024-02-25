69520bcbbaa6c40eaa0d141f4ba90ad308155a9d
tot <126389606+totkyle@users.noreply.github.com>
2024-02-25 13:55:21 +0300

    Update Repos.svelte

--- src/components/organisms/Repos.svelte
+++ src/components/organisms/Repos.svelte
@@ -5,7 +5,7 @@
 	let repos: Repo[];
 
 	onMount(async () => {
-		const response = await fetch('https://gh-pinned-repos.egoist.dev/?username=totkyle');
+		const response = await fetch('https://github-pinned-repositories.vercel.app/totkyle');
 		repos = await response.json();
 	});
 </script>
